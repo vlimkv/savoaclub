439f92f3009c23fcfe9f0713d750c4a4cf086839
vlimkv <alimkhanslambek@mail.ru>
2025-05-17 10:59:11 +0500

    Vercel vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), 
-    },
-  },
-  server: {
-    port: 3000,
-    open: true, 
-  },
+  base: '/',
   build: {
-    outDir: "dist", 
-    sourcemap: false,
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"), 
-    },
+    outDir: 'dist',
   },
 });
