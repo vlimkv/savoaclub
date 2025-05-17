fb252d90aaf472715241253c557bf040ad91d74a
vlimkv <alimkhanslambek@mail.ru>
2025-05-17 10:45:13 +0500

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), 
+    },
+  },
+  server: {
+    port: 3000,
+    open: true, 
+  },
+  build: {
+    outDir: "dist", 
+    sourcemap: false,
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"), 
+    },
+  },
+});
